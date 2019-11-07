f26dca0eb778429591b8eb8242ede38aad274bb7
Feldkircher <FELDKIC@ccf.org>
2019-11-07 10:30:55 -0500

    commit

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/user.model');
+require('./services/passport.services');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 *1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-//route handler
-app.get('/',(req, res) => {
-    res.send({hi: 'dude'});
-});
+require('./routes/auth.routes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
